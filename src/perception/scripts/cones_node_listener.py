43eb69aa5f0733e20121d59643dc8f44164d7149
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2023-04-25 04:28:36 +0200

    - generate the output images for the rosbag inside the experiment folder in `yolov5/runs/detect/exp` after running:  - `conda activate yolov5`  - in one terminal run: `source devel/setup.sh  && rosrun perception cones_node_listener.py`  - in another terminal run: `source devel/setup.sh && rosrun perception cones_node_test_talker.py`

--- src/perception/scripts/cones_node_listener.py
+++ src/perception/scripts/cones_node_listener.py
@@ -7,15 +7,26 @@ from first_package.msg import kartTemperatures
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+from yolov5.detect import *
 
 def callback(data):
     global idx
+    global yolo_detect
+
     rospy.loginfo("Images:{}".format(idx))
-    cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+    cv2_image_bgr = bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
+    img_full_path = "imgs/frames%d.png" % idx
+    yolo_detect.run(cv2_image_bgr, idx, img_full_path)
     # cv2.imwrite("imgs/frame%d.jpg" % idx, cv_image)
     idx += 1
 
 def listener(camera_publisher_topic):
+    global yolo_detect
+    # Initialize yolo
+    opt = parse_opt()
+    yolo_detect = YoloDetect(**vars(opt))
+    
+
     rospy.init_node('cones_node_listener', anonymous=False)
     rospy.Subscriber(camera_publisher_topic, Image, callback)
     print("listener started")
